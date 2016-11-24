5bb456da86b00edf580f81583d7ebe7d9eacebe6
Nathan P. Keene <npk@txtincorporated.com>
2016-11-24 00:33:29 -0800

    Remove merge debris

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const http = require('http');
 
-<<<<<<< HEAD
-const app = require('./lib/app');
-
-http.createServer(app).listen(8888);
-=======
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
@@ -14,6 +9,3 @@ const server = http.createServer(app);
 server.listen(port, () => {
   console.log('Server running at ', server.address());
 });
-  
-
->>>>>>> txtincDev
